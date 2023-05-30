5c879fa26b7cbe8df7645807d9154ce75c4bda8d
tianzhiyuan <286964436@qq.com>
2023-05-30 14:47:47 +0800

    get

---
+++ app.js
@@ -0,0 +1,19 @@
+const koa = require('koa')
+const cors = require('koa2-cors')
+var app = new koa()
+var proxy = require('koa-proxy')
+
+app.use(cors({
+    origin: 'http://localhost:1234',
+    maxAge: 5,
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'DELETE', 'OPTIONS', 'PUT'],
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'Accept-Custom-Type', 'OAUTH2', 'zx-oauth2','Content-Encoding']
+}))
+
+app.use(proxy({
+    host: 'http://office.zx-tech.net:8011',
+}));
+
+
+app.listen(3000)
